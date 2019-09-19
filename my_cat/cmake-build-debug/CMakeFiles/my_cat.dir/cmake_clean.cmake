70eb20a01d6299a7c4e3bad1f6c8a2697e37e5fe
Arv1k <you@example.com>
2019-09-19 16:34:43 +0300

    Some changes.

---
+++ my_cat/cmake-build-debug/CMakeFiles/my_cat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_cat.dir/main.cpp.o"
+  "my_cat.pdb"
+  "my_cat"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_cat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ numb/cmake-build-debug/CMakeFiles/numb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/numb.dir/main.cpp.o"
+  "numb.pdb"
+  "numb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/numb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

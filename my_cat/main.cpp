3170f254d26d285bbaf885b455743897b36e95dd
Arv1k <you@example.com>
2019-09-19 19:04:21 +0300

    I've done my_cat!

--- my_cat/main.cpp
+++ my_cat/main.cpp
@@ -1,49 +1,71 @@
 #include <unistd.h>
 #include <malloc.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 
 
+
 enum {
-    MAX_STR_SIZE = 1024,
+    MAX_STR_SIZE = 1024*1024,
     STD_OUT      = 0,
     STD_IN       = 1,
 };
 
 
+int save_write(char* buf, int ch_read);
+
+
 int main(int argc, char** argv) {
-    size_t num_files = 0;
+    int num_files = (argc > 1) ? (argc - 1) : 1;
 
-    num_files = (argc > 1) ? (argc - 1) : 1;
+    for (int i = 1; i <= num_files; i++) {
+        int in_fd = 0;
 
-    for (size_t i = 1; i <= num_files; i++) {
-        size_t in_fd = 0;
-        size_t size  = 0;
-
-        if (argc == 1) {
+        if (argc == 1)
             in_fd = STD_IN;
-            size  = MAX_STR_SIZE;
-        }
         else {
             in_fd = open(argv[i], O_RDONLY);
+            if (in_fd < 0) {
+                perror("Can't open the file! ");
 
-            struct stat file_stat = {};
-            fstat(in_fd, &file_stat);
-
-            size = file_stat.st_size;
+                continue;
+            }
         }
 
-        char *buf = (char *) calloc(size, 1);
+        for (;;) {
+            char buf[MAX_STR_SIZE];
+            int ch_read = read(in_fd, buf, MAX_STR_SIZE);
+            if(ch_read < 0) {
+                perror("Can't read the data! ");
 
-        size_t ch_read = read(in_fd, buf, MAX_STR_SIZE);
-        while (ch_read) {
-            write(STD_OUT, buf, ch_read);
-            ch_read = read(in_fd, buf, MAX_STR_SIZE);
+                break;
+            }
+
+            if (ch_read == 0) break;
+
+            int ch_write = save_write(buf, ch_read);
+            if(ch_write < 0) {
+                perror("Can't write the data! ");
+
+                break;
+            }
         }
-        printf("\n");
-
 
         close(in_fd);
-        free(buf);
+    }
+}
+
+int save_write(char* buf, int ch_read) {
+    int ch_write     = 0;
+    int ch_to_write  = ch_read;
+    int cur_ch_write = 0;
+
+    for (;;) {
+        cur_ch_write = write(STD_OUT, &buf[ch_write], ch_to_write);
+
+        if (ch_write == ch_read) return ch_write;
+        if (cur_ch_write < 0)    return -1;
+
+        ch_write    += cur_ch_write;
+        ch_to_write -= ch_write;
     }
 }
